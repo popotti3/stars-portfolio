e2ffd2015cb9257a81120205d4db3a95c5c04403
popotti2 <aapo1anttila@gmail.com>
2023-04-18 18:20:55 +0300

    ghpages

---
+++ src/image.js
@@ -0,0 +1,6 @@
+
+function Image() {
+    return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTinZwqziUI9jcyTugTZiihBeIU-0ZARmrucw&usqp=CAU"/>;
+  }
+
+  export default Image;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom/client';
 import Omakuva from "./omakuva.js"
 import Header from "./header.js"
 import Element from "./element.js"
+import FluidGrid from 'react-fluid-grid'
 import "./style.css"
 
 
 const myPage = <div>
 <Header/>
-<Element projekti="Testiprojekti"
-image="https://cdn.pixabay.com/photo/2020/03/21/14/45/moon-4954228_1280.jpg"
-text="Tämä on minun tekeleeni vähän väsyneenä"
-link="https://unity.com/"/>
-</div>
+<div class="fluid_container">
+    <FluidGrid>
+        <Element projekti="Testiprojekti"
+        image="https://cdn.pixabay.com/photo/2020/03/21/14/45/moon-4954228_1280.jpg"
+        text="Tämä on minun tekeleeni vähän väsyneenä"
+        link="https://unity.com/"/>
 
+
+
+        <Element projekti="Testi2"
+        image="https://cdn.pixabay.com/photo/2020/03/21/14/45/moon-4954228_1280.jpg"
+        text="Tämä on minun tekeleeni vähän virkeämpänä"
+        link="https://unity.com/"/>
+    </FluidGrid>
+</div>
+</div>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myPage);
